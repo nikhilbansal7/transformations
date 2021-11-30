70b8e44e58dc8057b676454c28f650ad0ddbe4d2
Nikhil Bansal <nikhil.bansal@hevodata.com>
2021-11-30 21:09:31 +0530

    source path clean

--- source_path.py
+++ source_path.py
@@ -1,20 +1,7 @@
 from io.hevo.api import Event
 
-"""
-event: each record streaming through Hevo pipeline is an event
-
-returns: 
-    - The modified event object.
-    - Array of event objects if new events are generated from the incoming event.
-    - None if the event is supposed to be dropped from the pipeline.
-
-Read complete documentation at: https://docs.hevodata.com/pipelines/transformations/
-"""
-
 
 def transform(event):
-    # Get event name from the event #
-    # eventName = event.getEventName()
 
     # Get properties from the event #
     properties = event.getProperties()
@@ -25,9 +12,6 @@ def transform(event):
         else :
             count+=1
     properties['source_directory'] = properties['__hevo_id'][:-(count+1)]
-
-    # Rename event
-    # event.rename(newName)
 
     return event
 
